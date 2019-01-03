2fe87b453616c4e579a5e8a3ba8d1bce61b66001
Aleksey <lesha.leshka@inbox.ru>
2019-01-04 00:01:41 +0300

    The create project view

--- tests/Feature/ManageProjectsTest.php
+++ tests/Feature/ManageProjectsTest.php
@@ -6,30 +6,19 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ProjectsTest extends TestCase
+class ManageProjectsTest extends TestCase
 {
     use WithFaker, RefreshDatabase;
 
     /** @test */
-    public function guests_cannot_create_projects()
-    {
-        $attributes = factory('App\Project')->raw();
-
-        $this->post('/projects', $attributes)->assertRedirect('login');
-    }
-
-    /** @test */
-    public function guests_cannot_view_projects()
-    {
-        $this->get('/projects')->assertRedirect('login');
-    }
-
-    /** @test */
-    public function guests_cannot_view_a_single_project()
+    public function guests_cannot_manage_projects()
     {
         $project = factory('App\Project')->create();
 
+        $this->get('/projects')->assertRedirect('login');
+        $this->get('/projects/create')->assertRedirect('login');
         $this->get($project->path())->assertRedirect('login');
+        $this->post('/projects', $project->toArray())->assertRedirect('login');
     }
 
     /** @test */
@@ -38,6 +27,8 @@ class ProjectsTest extends TestCase
         $this->withoutExceptionHandling();
 
         $this->actingAs(factory('App\User')->create());
+
+        $this->get('/projects/create')->assertOk();
         
         $attributes = [
             'title' => $this->faker->sentence,
@@ -72,7 +63,7 @@ class ProjectsTest extends TestCase
 
         $project = factory('App\Project')->create();
 
-        $this->get($project->path())->assertStatus(403);
+        $this->get($project->path())->assertForbidden();
     }
 
     /** @test */
