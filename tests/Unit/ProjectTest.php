6bcc2b02e837545d7cc6644ddc003252ea94c021
Aleksey <lesha.leshka@inbox.ru>
2019-03-05 15:11:06 +0300

    Layered Tests and Invitations

--- tests/Unit/ProjectTest.php
+++ tests/Unit/ProjectTest.php
@@ -2,13 +2,14 @@
 
 namespace Tests\Unit;
 
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ProjectTest extends TestCase
 {
     use RefreshDatabase;
-    
+
     /** @test */
     public function it_has_a_path()
     {
@@ -34,5 +35,15 @@ class ProjectTest extends TestCase
 
         $this->assertCount(1, $project->tasks);
         $this->assertTrue($project->tasks->contains($task));
+    }
+
+    /** @test */
+    public function it_can_invite_a_user()
+    {
+        $project = factory('App\Project')->create();
+
+        $project->invite($user = factory(User::class)->create());
+
+        $this->assertTrue($project->members->contains($user));
     }
 }
