b0a149ccf30485e9e394a5c3085924f8574ffe0b
Aleksey <lesha.leshka@inbox.ru>
2019-04-16 17:55:44 +0300

    Object-Oriented JavaScript

--- resources/js/components/NewProjectModal.vue
+++ resources/js/components/NewProjectModal.vue
@@ -12,10 +12,12 @@
                             type="text"
                             id="title"
                             class="border p-2 text-xs block w-full rounded"
-                            :class="errors.title ? 'border-error' : 'border-muted-light'"
+                            :class="form.errors.title ? 'border-error' : 'border-muted-light'"
                             v-model="form.title">
 
-                        <span class="text-xs italic text-error" v-if="errors.title" v-text="errors.title[0]"></span>
+                        <span class="text-xs italic text-error"
+                              v-if="form.errors.title"
+                              v-text="form.errors.title[0]"></span>
                     </div>
 
                     <div class="mb-4">
@@ -24,10 +26,12 @@
                         <textarea
                             id="description"
                             class="border border-muted-light p-2 text-xs block w-full rounded"
+                            :class="form.errors.description ? 'border-error' : 'border-muted-light'"
                             rows="7"
                             v-model="form.description"></textarea>
 
-                        <span class="text-xs italic text-error" v-if="errors.description" v-text="errors.description[0]"></span>
+                        <span class="text-xs italic text-error" v-if="form.errors.description"
+                              v-text="form.errors.description[0]"></span>
                     </div>
                 </div>
 
@@ -46,7 +50,8 @@
                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" class="mr-2">
                             <g fill="none" fill-rule="evenodd" opacity=".307">
                                 <path stroke="#000" stroke-opacity=".012" stroke-width="0" d="M-3-3h24v24H-3z"></path>
-                                <path fill="#000" d="M9 0a9 9 0 0 0-9 9c0 4.97 4.02 9 9 9A9 9 0 0 0 9 0zm0 16c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7zm1-11H8v3H5v2h3v3h2v-3h3V8h-3V5z"></path>
+                                <path fill="#000"
+                                      d="M9 0a9 9 0 0 0-9 9c0 4.97 4.02 9 9 9A9 9 0 0 0 9 0zm0 16c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7zm1-11H8v3H5v2h3v3h2v-3h3V8h-3V5z"></path>
                             </g>
                         </svg>
 
@@ -64,29 +69,33 @@
 </template>
 
 <script>
+    import BirdboardForm from './BirdBoardForm';
+
     export default {
         data() {
             return {
-                form: {
+                form: new BirdboardForm({
                     title: '',
                     description: '',
                     tasks: [
                         { body: ''},
                     ]
-                },
-                errors: {}
+                })
             };
         },
+
         methods: {
             addTask() {
-                this.form.tasks.push({value: ''});
+                this.form.tasks.push({ body: '' });
             },
+
             async submit() {
-                try {
-                    location = (await axios.post('/projects', this.form)).data.message;
-                } catch (error) {
-                    this.errors = error.response.data.errors;
+                if (! this.form.tasks[0].body) {
+                    delete this.form.originalData.tasks;
                 }
+
+                this.form.submit('/projects')
+                    .then(response => location = response.data.message);
             }
         }
     }
